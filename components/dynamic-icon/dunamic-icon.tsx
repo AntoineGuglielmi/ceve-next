36aa6dbbc29bf4c84003750bd6e0d167889b2e0d
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-02-13 10:53:07 +0100

    002 - Create dynamic icon component

---
+++ components/dynamic-icon/dunamic-icon.tsx
@@ -0,0 +1,14 @@
+import * as LucideIcons from 'lucide-react'
+import { LucideIcon, LucideProps } from 'lucide-react'
+
+interface DynamicIconProps extends LucideProps {
+  name: keyof typeof LucideIcons
+}
+
+export function DynamicIcon({ name, ...props }: DynamicIconProps) {
+  const Icon = LucideIcons[name] as LucideIcon
+
+  if (!Icon) return null
+
+  return <Icon {...props} />
+}
