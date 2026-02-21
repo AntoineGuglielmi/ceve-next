ab7f2d280b2159b2af6fa480dcf087245e7b1d75
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-02-21 01:35:06 +0100

    master - Implement get-types

---
+++ types/strapi-types.ts
@@ -0,0 +1,95 @@
+
+export interface Info {
+  id?: number;
+  documentId?: string;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  publishedAt?: Date | string;
+  locale?: string | null;
+  firstname?: string;
+  lastname?: string;
+  address1?: string;
+  address2?: string;
+  postalCode?: string;
+  city?: string;
+  phone?: string;
+  email?: string;
+  website?: string;
+};
+
+export interface Media {
+  id: number;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: { thumbnail: MediaFormat; small: MediaFormat; medium: MediaFormat; large: MediaFormat; };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string;
+  provider: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface MediaFormat {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  width: number;
+  height: number;
+  size: number;
+  path: string;
+  url: string;
+}
+
+export interface User {
+  id?: number;
+  username: string;
+  email: string;
+  provider?: string;
+  confirmed?: boolean;
+  blocked?: boolean;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  role: Role | null | number;
+};
+
+export interface Role {
+  id?: number;
+  documentId?: string;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  name: string;
+  description: string;
+  type: string;
+};
+
+export interface FindOne<T> {
+  data: T;
+  meta: {
+    pagination?: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    };
+  };
+};
+
+export interface FindMany<T> {
+  data: T[];
+  meta: {
+    pagination?: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    };
+  };
+};
