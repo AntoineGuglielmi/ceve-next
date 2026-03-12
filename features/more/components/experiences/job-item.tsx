be5ddfdc78294cc8123a5862dce96e4e496c54da
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-03-13 00:59:29 +0100

    014 - Integrate markdown

--- features/more/components/experiences/job-item.tsx
+++ features/more/components/experiences/job-item.tsx
@@ -1,4 +1,6 @@
+import Markdown from '@/shared/components/markdown'
 import { MoleculesJobItem } from '@/types/strapi-types'
+import ReactMarkdown from 'react-markdown'
 
 type JobItemProps = {
   className?: string
@@ -10,13 +12,15 @@ export default function JobItem({
   children,
   text,
   title,
+  description,
+  technos,
 }: JobItemProps) {
   return (
     <div
       className={`JobItem bg-cv-gris-clair/25 rounded-lg p-4 ${className ?? ''}`}
     >
-      <p>{title}</p>
-      {children}
+      <p className="font-bold border-b pb-2 mb-2">{title}</p>
+      <Markdown content={description!} />
     </div>
   )
 }
