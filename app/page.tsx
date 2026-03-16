f0c6fba8b77b64a53d1a40b606bb0515ecc1da9a
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-03-16 13:55:34 +0100

    master - Add force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ type MorePageProps = {
   params: Promise<void>
 }
 
+export const dynamic = 'force-dynamic'
+
 export default function MorePage({}: MorePageProps) {
   return (
     <main className="flex @container/main flex-col text-cv-anthracite w-full max-w-[21cm] mx-auto shadow-[0_0_0.5rem_0_rgba(0,0,0,0.15)] bg-cv-blanc ">
