e344dfe8e2957a366ec93724d7eb97a5a3edcb7f
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-04-03 21:27:23 +0200

    feature/new-cv-skin - Return 404 if no skin is set in admin

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import GoToCv from '@/features/skins/go-to-cv'
 import { ServiceGetConfig } from '@/services/config'
-import { redirect } from 'next/navigation'
+import { notFound } from 'next/navigation'
 
 type MorePageProps = {
   params: Promise<void>
@@ -11,7 +11,7 @@ export const dynamic = 'force-dynamic'
 export default async function MorePage({}: MorePageProps) {
   const { skin } = await ServiceGetConfig()
   if (!skin) {
-    // redirect('/')
+    notFound()
   }
 
   const { code: skinCode } = skin!
@@ -20,7 +20,7 @@ export default async function MorePage({}: MorePageProps) {
     `./skins/[code]/${skinCode}/PublicView`
   ).then((mod) => mod.default)
   return (
-    <main className="flex @container/main flex-col text-cv-anthracite w-full max-w-240 mx-auto shadow-[0_0_0.5rem_0_rgba(0,0,0,0.15)] bg-cv-blanc ">
+    <main className="flex @container/main flex-col text-cv-anthracite w-full max-w-240 mx-auto bg-cv-blanc ">
       {process.env.NODE_ENV === 'development' && <GoToCv />}
       <SelectedSkin />
     </main>
